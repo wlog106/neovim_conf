220c74be117c19daec01aebe7d85ff9ef7398fc6
wlog <xuanhao8765@gmail.com>
2025-05-03 16:41:59 +0800

    update: alpha nvim ascii art

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,28 +1,26 @@
 return {
-  'goolord/alpha-nvim',
-  config = function ()
-    -- ~/.config/nvim/lua/plugins/alpha.lua (example path)
-    local alpha = require('alpha')
-    local dashboard = require('alpha.themes.dashboard')
+    'goolord/alpha-nvim',
+    config = function ()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
 
+      dashboard.section.header.val = {
+        "                                                     ",
+        "                                                     ",
+        "                                                     ",
+        "                                                     ",
+        "                                                     ",
+        "                                                     ",
+        "                                                     ",
+        "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+        "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+        "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+        "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+        "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+        "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+        "                                                     ",
+      }
 
-    dashboard.section.header.val = {
-
-      [[]],
-      [[  ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗  ]],
-      [[  ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║  ]],
-      [[  ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║  ]],
-      [[  ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║  ]],
-      [[  ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║  ]],
-      [[  ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝  ]],
-    }
-
-    -- You can replace this with your own header or icon configuration.
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  New File", ":ene<CR>"),
-      dashboard.button("f", "  Find File", ":Telescope find_files<CR>"),
-    }
-
-    alpha.setup(dashboard.opts)
-  end
+      alpha.setup(dashboard.opts)
+    end
 }
